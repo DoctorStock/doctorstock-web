9f06ffe3b3bb6919ccf47230a086783ef0be4d07
Youbeen Cho <ybcho97@naver.com>
2025-10-16 17:55:27 +0900

    [Add] 로그인 페이지 기본 구조 및 에셋 추가
    - globals.css 코드 추가
    - 로그인 페이지 layout.tsx 구조 작업
    - public 폴더 생성 및 로고 에셋 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,13 @@
+import styles from './page.module.css';
+
 export default function Login() {
-  return <div>Login</div>;
+  return <div>
+            <h1>
+            <img
+              src="/assets/logo.svg"   
+              alt="닥터스톡로고"
+              className={styles.logo}
+            />
+          </h1>
+  </div>;
 }
