b65313997ff2575faafcea7426c37508a2dfdcc6
Youbeen Cho <ybcho97@naver.com>
2025-10-22 18:16:37 +0900

    [Add] 에셋 및 기본 모달 컴포넌트 추가
    - 로그인 에셋 추가
    - BasicModal 컴포넌트 추가
    - 로그인 페이지 ui 작업

---
+++ app/login/components/BasicModal.tsx
@@ -0,0 +1,25 @@
+import { on } from "events";
+import { defaultOverrides } from "next/dist/server/require-hook";
+
+interface BasicModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  message: string;
+}
+
+export default function BasicModal({
+  isOpen,
+  onClose,
+  message,
+}: BasicModalProps) {
+  if (!isOpen) return null;
+
+  return (
+    <div>
+      <div>
+        <p>{message}</p>
+        <button onClick={onClose}>확인</button>
+      </div>
+    </div>
+  );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,63 @@
-import styles from './page.module.css';
+"use client";
+
+import { useState } from "react";
+import BasicModal from "./components/BasicModal";
+import styles from "./page.module.css";
 
 export default function Login() {
-  return <div>
-            <h1>
-            <img
-              src="/assets/logo.svg"   
-              alt="닥터스톡로고"
-              className={styles.logo}
+  const [userId, setUserId] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      <div>
+        <h1>
+          <img
+            src="/assets/logo.svg"
+            alt="닥터스톡로고"
+            className={styles.logo}
+          />
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="userId">아이디</label>
+            <input
+              type="text"
+              id="userId"
+              value={userId}
+              onChange={(e) => {
+                setUserId(e.target.value);
+              }}
+              placeholder="아이디를 입력해 주세요."
             />
-          </h1>
-  </div>;
+          </div>
+          <div>
+            <label htmlFor="userPassword">비밀번호</label>
+            <input
+              id="userPassword"
+              type={showPassword ? "text" : "password"}
+              value={userPassword}
+              onChange={(e) => {
+                setUserPassword(e.target.value);
+              }}
+              placeholder="비밀번호를 입력해 주세요."
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              aria-label={showPassword ? "비밀번호 숨기기" : "비밀번호 보이기"}
+            >
+              {showPassword ? (
+                <img src="/assets/shown.png" alt="비밀번호 보이기" />
+              ) : (
+                <img src="/assets/unshown.png" alt="비밀번호 숨기기" />
+              )}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
