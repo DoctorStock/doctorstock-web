cc43178517b890382c39ccaf20fd16711c94d758
YoubeenCho <ybcho97@naver.com>
2025-10-29 22:45:27 +0900

    [test]test

--- app/login/components/PasswordResetModal.tsx
+++ app/login/components/PasswordResetModal.tsx
@@ -51,7 +51,7 @@ export default function BasicModal({
   } else if (confirmPassword === newPassword) {
     confirmMessage = "비밀번호가 일치합니다.";
   } else {
-    confirmMessage = "비밀번호가 불일치합니다.";
+    confirmMessage = "비밀번호가 불일치합니다다다.";
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
