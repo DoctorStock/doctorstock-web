24a89f1936605ee941f16da97320051ca910bd72
김건형 <show11837@gmail.com>
2025-11-20 17:25:25 +0900

    [Feat] Header 알림 및 로그아웃 버튼 추가

--- app/components/layout/Header/page.tsx
+++ app/components/layout/Header/page.tsx
@@ -5,11 +5,21 @@ import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Header() {
+  // TODO: 로그아웃 로직 구현
+  const handleLogout = () => {
+    console.log('로그아웃');
+  };
+
+  // TODO: 알림 클릭 로직 구현
+  const handleNotification = () => {
+    console.log('알림 클릭');
+  };
+
   return (
     <div className={styles.header}>
       <Link href="/" className={styles.logo}>
         <Image
-          src="/assets/doctorStock_icon.png"
+          src="/assets/logo.svg"
           alt="Doctor Stock"
           width={26}
           height={26}
@@ -17,6 +27,28 @@ export default function Header() {
         />
         <span className={styles.logoText}>Doctor Stock</span>
       </Link>
+      
+      <div className={styles.rightSection}>
+        <button
+          onClick={handleNotification}
+          className={styles.notificationButton}
+          aria-label="알림"
+        >
+          <Image
+            src="/assets/bell_on.svg"
+            alt="알림"
+            width={36}
+            height={36}
+          />
+        </button>
+
+        <button
+          onClick={handleLogout}
+          className={styles.logoutButton}
+        >
+          로그아웃
+        </button>
+      </div>
     </div>
   );
 }
