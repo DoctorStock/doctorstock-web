6a1162b076dabec3fd294ce68c69fe5c2afacd28
김건형 <show11837@gmail.com>
2025-11-25 13:53:32 +0900

    [Feat] Sidebar 영역 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import styles from "./page.module.css";
 import Header from "./components/layout/Header/page";
+import Sidebar from "./components/layout/Sidebar/page";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header/>
-      <div>메인 페이지</div>
+      <div className={styles.content}>
+        <Sidebar/>
+        <main className={styles.main}>
+          {/* 메인 콘텐츠 영역 */}
+        </main>
+      </div>
     </div>
   );
 }
