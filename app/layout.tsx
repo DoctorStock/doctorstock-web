84472d91bf7bd0ecb047e153bf3eafa0e0b07385
김건형 <show11837@gmail.com>
2025-11-25 11:58:47 +0900

    [Hotfix] 폰트 폴더 경로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,22 +7,22 @@ import "./theme.css";
 const pretendard = localFont({
   src: [
     {
-      path: './fonts/Pretendard-Regular.woff2',
+      path: './components/fonts/Pretendard-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: './fonts/Pretendard-Medium.woff2',
+      path: './components/fonts/Pretendard-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: './fonts/Pretendard-SemiBold.woff2',
+      path: './components/fonts/Pretendard-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: './fonts/Pretendard-Bold.woff2',
+      path: './components/fonts/Pretendard-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
